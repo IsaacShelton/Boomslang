5e420d0a2e181e72f786165d851593d75f9d4a6c
IsaacShelton <isaac.a.shelton@gmail.com>
2016-03-27 11:38:17 -0500

    Updating Copyright Notices

--- include/assemble.h
+++ include/assemble.h
@@ -1,7 +1,25 @@
 
-
 #ifndef ASSEMBLE_H_INCLUDED
 #define ASSEMBLE_H_INCLUDED
+
+/*
+    (c) 2016 Isaac Shelton
+
+    This file is part of Boomslang.
+
+    Boomslang is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Boomslang is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Boomslang. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 using namespace std;
 
