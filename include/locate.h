9c5cfa5c6a5a81005b18ad29fbb95dfbf2e42661
Isaac Shelton <isaac.a.shelton@gmail.com>
2016-08-27 16:58:10 -0500

    Added Linux Test Constants

--- include/locate.h
+++ include/locate.h
@@ -23,13 +23,6 @@
 
 #include <string>
 
-#if defined(__WIN32__)
-#define HOME          std::string("C:\\Boomslang\\source\\")
-#define PACKAGEHOME   std::string("C:\\Boomslang\\packages\\")
-#define COREHOME      std::string("C:\\Boomslang\\core\\")
-#define LOGHOME       std::string("C:\\Boomslang\\logs\\")
-#define RESOURCEHOME  std::string("C:\\Boomslang\\resources\\")
-#define MINGWHOME     std::string("C:\\Boomslang\\resources\\MinGW\\")
 #define CPP_SOURCE    std::string("source.cpp")
 #define CPP_HEADER    std::string("source.h")
 #define CPP_OBJECT    std::string("source.o")
@@ -37,6 +30,21 @@
 #define LOG_LEXER     std::string("lexer.log")
 #define LOG_ENFORCER  std::string("enforcer.log")
 #define LOG_ASSEMBLER std::string("assembler.log")
+
+#if defined(__WIN32__)
+#define HOME          std::string("C:\\Boomslang\\source\\")
+#define PACKAGEHOME   std::string("C:\\Boomslang\\packages\\")
+#define COREHOME      std::string("C:\\Boomslang\\core\\")
+#define LOGHOME       std::string("C:\\Boomslang\\logs\\")
+#define RESOURCEHOME  std::string("C:\\Boomslang\\resources\\")
+#define MINGWHOME     std::string("C:\\Boomslang\\resources\\MinGW\\")
+#elif defined(__WIN32__)
+#define HOME          std::string("/home/isaac/Boomslang/source/")
+#define PACKAGEHOME   std::string("/home/isaac/Boomslang/packages/")
+#define COREHOME      std::string("/home/isaac/Boomslang/core/")
+#define LOGHOME       std::string("/home/isaac/Boomslang/logs/")
+#define RESOURCEHOME  std::string("/home/isaac/Boomslang/resources/")
+#define MINGWHOME     std::string("/home/isaac/Boomslang/resources/MinGW/")
 #endif
 
 extern std::string USERNAME;
